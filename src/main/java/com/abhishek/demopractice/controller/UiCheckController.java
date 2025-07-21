7a7f83e3e492e462ea03843cf3a561dd4087ece4
Abhi <abimunda09@gmail.com>
2025-07-21 06:35:05 +0530

    front page updated to apis

--- src/main/java/com/abhishek/demopractice/controller/UiCheckController.java
+++ src/main/java/com/abhishek/demopractice/controller/UiCheckController.java
@@ -5,13 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class UiCheckController {
 
-    @GetMapping
-    public String getFirstPage(){
-        return new String("Hello!🙂 The site is working perfectly.\n\n fetch through apis \n api/doGetCalendarFreeDays/{doctor}/{appointmentType} \n api/bookAppointment/{doctor}/{appointmentType}/{appoitnmentDate}/{startTime}/{endTime}/{firstName}/{lastName}/{email}/{dateOfBirth}/{phoneNumber} "); 
+    @GetMapping(value = "/", produces = "text/html")
+    public String getFirstPage() {
+        return """
+                <html>
+                    <body>
+                        <h2>Hello! 🙂</h2>
+                        <p>The site is working perfectly.</p>
+                        <br>
+                        <br>
+                        <p><strong>Fetch through APIs:</strong></p>
+                        <ul>
+                            <li>/api/doGetCalendarFreeDays/{doctor}/{appointmentType}</li>
+                            <li>/api/bookAppointment/{doctor}/{appointmentType}/{appointmentDate}/{startTime}/{endTime}/{firstName}/{lastName}/{email}/{dateOfBirth}/{phoneNumber}</li>
+                        </ul>
+                    </body>
+                </html>
+                """;
     }
-
 
 }
